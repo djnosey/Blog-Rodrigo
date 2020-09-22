6efcd51954af76f22b0828c15c2ce11252675a05
steven payne <djnosey@hotmail.co.uk>
2020-09-22 15:21:51 +0200

    starting side bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ArticleCard from './ArticleCard';
-import Navbar from './Navbar';
-import "./app.css"
-
+import React from "react";
+import ArticleCard from "./ArticleCard";
+import Navbar from "./Navbar";
+import "./app.css";
 
 //BEM
 
 function App() {
   return (
     <div className="app">
-        <Navbar />
-        <div className ="app__left">
+      <Navbar />
+      <div className="app__container">
+      <div className="app__left">
         <ArticleCard />
         <ArticleCard />
         <ArticleCard />
-        </div>
-        {/*left side*/}
-        
-        {/*right side*/}
-        {/*sidebar*/}
+      </div>
+      <div className="app__right">
+        <ArticleCard />
+        <ArticleCard />
+      </div>
+      </div>
+      
     </div>
   );
 }
